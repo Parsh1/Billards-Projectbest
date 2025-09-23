8c239ac9deb94f1fa4de407d4e91b0e9df6dcc6d
mykeEPFL <miguel.grandeferrando@epfl.ch>
2025-09-23 11:09:12 +0200

    Parsh, I've done some minor changes: Take a look at them before saving them and tell them if you think they're ok.
    - Changed , by ; in the for(...)
    - Added ; after fopen()
    - Changed the parameters in the fread() function: Just passed fp in the last argument and then checked if fp != 2
    - Changed the 0 <= Largeur <= 100 by Largeur < 100 . I haven't checked if it's < or <= but I'm quite sure that  0 <= Largeur <= 100 doesn't work in C
    
    Let me know what do you think. These changes could be wrong... I don'know...

--- Billards-App/Billards-App/Program.c
+++ Billards-App/Billards-App/Program.c
@@ -12,39 +12,31 @@ int main(int argc, char* argv[]) {
 
 	// The above makes sure we recieved the correct number of lines for parameters
 
-	for (int i = 5, i < 29, i++) {
+	for (int i = 5; i < 29; i++) {
 		if (argv[i] < 0 || argv[i] > 255) {
 			printf("There is an error in the color parameter values");
 			exit(1);
 		}
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	FILE *fp = fopen("pixmap.bin", "rb") // Reads file and puts the file reader in binary mode 
-		if (!fp) {						// Makes sure file is there 
-			perror("File open failed");	// Message to tell us where the error is
-				return 1;
-		}
+	FILE *fp = fopen("pixmap.bin", "rb"); // Reads file and puts the file reader in binary mode 
+	if (!fp) {						// Makes sure file is there 
+		perror("File open failed");	// Message to tell us where the error is
+		return 1;
+	}
 
-	int Table[2];						// Add the table from memory allocation here instead but using this tentatively
-	if (fread(Table, sizeof(int), 2, fp != 2) {	// Checks to make sure it has read 2 elements 
-		perror ("Failed to read the width and length");		// Error code
+	int Table[2];
+	(fread(Table, sizeof(int), 2, fp)
+	// Add the table from memory allocation here instead but using this tentatively
+	if (fp != 2) {	// Checks to make sure it has read 2 elements 
+		perror ("Failed to read the width and length");  // Error code
 		fclose(fp);										// Closes file to do further checks before proceeding however, 
 		return 1;										// this can be combined with the next step later if slow
 	}
 	int Largeur = Table [0];			// Replace with the 0th entry of the malloc table we make
 	int Hauteur = Table [1];			
 
-		if (Largeur > 1000 || 0 <= Largeur <= 100 || Hauteur > 100 || 0 <= Hauteur <= 100) {
+		if (Largeur > 1000 || Largeur < 100 || Hauteur > 100 || Hauteur < 100) {
 			printf (" The bounds provided are invalid so the program cannot continue");
 			exit(1);
 		}
