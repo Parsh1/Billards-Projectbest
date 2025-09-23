9a997cfefa247fa921a097b322e67fe8b218f7db
Parsh1 <parsh.jain@epfl.ch>
2025-09-23 15:31:17 +0200

    Update Final.c

--- Billards-App/Billards-App/Final.c
+++ Billards-App/Billards-App/Final.c
@@ -111,6 +111,61 @@ void etape_4(int largeur, const unsigned int* a, int Lmin, int Lmax, int Cmin, i
 	fclose(fp);
 }
 
+int read_all_pixels(FILE *fp, unsigned int W, unsigned int H, unsigned int **outPix)
+{
+/*
+ Reads W*H pixels from the binary file.
+ Important: 'fp' is open in "rb" mode and positioned after reading Weight (W) and Height (H).
+
+It:
+   - Allocates a buffer with 'malloc' for W*H pixels
+   - Fills it in a single fread call
+   - Stores the pointer into *outPix and returns 0
+If there's an error:
+   - Prints an error message in stderr
+   - Frees the allocated memory
+   - Returns 1
+Also:
+   - If the file contains extra pixel, prints a warning
+     and ignores the extras.
+ */
+
+    // Checks if fp and outPix exist
+    if (!fp || !outPix) {
+        fprintf(stderr, "ERR: invalid arguments to read_all_pixels\n");
+        return 1;
+    }
+
+    // Compute tge total number of pixels
+    size_t total_pixels = (size_t)W * (size_t)H;
+
+    // Allocate a dynamic array of size_t using malloc
+    unsigned int *buffer = (unsigned int*)malloc(total_pixels * sizeof(unsigned int));
+    if (!buffer) { // checks malloc error
+        fprintf(stderr, "ERR: out of memory for %zu pixels\n", total_pixels);
+        return 1;
+    }
+
+    // Read all pixels
+    size_t pixels_read = fread(buffer, sizeof(unsigned int), total_pixels, fp);
+    if (pixels_read < total_pixels) { // checks if there are enough pixels
+        fprintf(stderr, "ERR: not enough pixels: got %zu need %zu\n", pixels_read, total_pixels);
+        free(buffer);
+        return 1; 
+    }
+
+    // Detect extra pixels
+    // If this succeeds, there are extra values in the file
+    unsigned int extra_probe;
+    if (fread(&extra_probe, sizeof(unsigned int), 1, fp) == 1) {
+        fprintf(stderr, "WARN: extra pixels detected (ignored)\n");
+    }
+
+    // Deliver the array with al the values by linkning it to the address *outPix
+    *outPix = buffer;
+    return 0;
+}
+
 int main(int argc, char* argv[]) {
 
 	if (argc != 30) {
