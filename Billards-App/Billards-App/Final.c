c4e1ab2d5e475732f97d170222b1833ee19d3974
Parsh1 <parsh.jain@epfl.ch>
2025-09-25 16:28:36 +0200

    Small changes

--- Billards-App/Billards-App/Final.c
+++ Billards-App/Billards-App/Final.c
@@ -174,21 +174,16 @@ int main(int argc, char* argv[]) {
 
 	// The above makes sure we received the correct number of lines for parameters
 
-	for (int i = 5, i < 29, i++) {
-		if (argv[i] < 0 || argv[i] > 255) {
-			printf("There is an error in the color parameter values");
-			exit(1);
-		}
 	}
 			
-	FILE *fp = fopen("pixmap.bin", "rb") // Reads file and puts the file reader in binary mode 
+	FILE *fp = fopen("Pixmap.bin", "rb") // Reads file and puts the file reader in binary mode 
 		if (!fp) {						// Makes sure file is there 
 			perror("File open failed");	// Message to tell us where the error is
 				return 1;
 		}
 
 	int Table[2];						// Add the table from memory allocation here instead, but using this tentatively
-	if (fread(Table, sizeof(int), 2, fp != 2) {	// Checks to make sure it has read 2 elements 
+	if ((fread(Table, sizeof(int), 2, fp) != 2) {	// Checks to make sure it has read 2 elements 
 		perror ("Failed to read the width and length");		// Error code
 		fclose(fp);										// Closes file to do further checks before proceeding, however, 
 		return 1;										// this can be combined with the next step later if slow
@@ -196,12 +191,8 @@ int main(int argc, char* argv[]) {
 	int Largeur = Table [0];			// Replace with the 0th entry of the malloc table we make
 	int Hauteur = Table [1];			
 
-		if (Largeur > 1000 || (0 <= Largeur && Largeur <= 100) || Hauteur > 100 || (0 <= Hauteur && Hauteur <= 100) {
+		if (Largeur > 1000 ||  Largeur <= 100 || Hauteur > 1000 || Hauteur <= 100) {
 			printf (" The bounds provided are invalid, so the program cannot continue");
-			exit(1);
-		}
-		if (Largeur < 0 || Hauteur < 0) {
-			printf("The bounds are negative so the program cannot continue");
 			exit(1);
 		}
 
